7992207d39d66d5fa30be24e68dc1bd26f5f1467
Carlos Durán <35487775+linchis@users.noreply.github.com>
2020-02-06 16:39:42 +0100

    Add files via upload

---
+++ Currency.kt
@@ -0,0 +1,8 @@
+
+data class Currency (
+	val type : String,
+	val error : Int,
+	val descError : String?,
+	val datosBasicos : DatosBasicos,
+	val listaComisiones : String?
+)
---
+++ CurrencyDTO.kt
@@ -0,0 +1,7 @@
+
+data class CurrencyDTO (
+	val error : Int,
+	val descError : String?,
+	val datosBasicos : DatosBasicosDTO,
+	val listaComisiones : String?
+)
